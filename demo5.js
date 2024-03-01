309139bb554837f86a181c2256042d7a3feedbe5
Esteban Abaroa <estebanabaroa@protonmail.com>
2024-03-01 23:01:59 +0000

    1

--- demo5.js
+++ demo5.js
@@ -1,32 +1,50 @@
-import { createHelia } from 'helia'
-import { libp2pDefaults } from './node_modules/helia/dist/src/utils/libp2p-defaults.browser.js'
+import { createLibp2p } from 'libp2p'
+import { multiaddr } from '@multiformats/multiaddr'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { webTransport } from '@libp2p/webtransport'
+import { mplex } from '@libp2p/mplex'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { noise } from '@chainsafe/libp2p-noise'
 import { bootstrap } from '@libp2p/bootstrap'
+import { identify } from '@libp2p/identify'
+import { kadDHT } from '@libp2p/kad-dht'
+import { webRTCDirect } from '@libp2p/webrtc'
 
 // example: /demo5.html?/ip4/127.0.0.1/udp/36338/quic-v1/webtransport/...
 const bootstrapMultiAddress = location.search.replace(/^\?/, '')
 
 ;(async () => {
-  // create libp2p options with pubsub and custom boostrap
-  const libp2pOptions = libp2pDefaults()
-  libp2pOptions.services.pubsub = gossipsub({allowPublishToZeroPeers: true})
-  // delete libp2pOptions.services.delegatedRouting
-  libp2pOptions.peerDiscovery = [bootstrap({list: [bootstrapMultiAddress]})]
-  // not sure why needed, doesn't connect without it
-  libp2pOptions.connectionGater = {denyDialMultiaddr: async () => false}
-
-  const helia = await createHelia({libp2p: libp2pOptions})
+  const libp2p = await createLibp2p({
+    peerDiscovery: [
+      bootstrap({list: [bootstrapMultiAddress]})
+    ],
+    transports: [
+      webTransport(),
+      webRTCDirect()
+    ],
+    streamMuxers: [yamux(), mplex()],
+    connectionEncryption: [noise()],
+    connectionGater: {
+      // not sure why needed, doesn't connect without it
+      denyDialMultiaddr: async () => false,
+    },
+    services: {
+      identify: identify(),
+      dht: kadDHT({}),
+      pubsub: gossipsub({allowPublishToZeroPeers: true})
+    }
+  })
 
   // pubsub sub
   const pubsubTopic = 'demo'
-  helia.libp2p.services.pubsub.addEventListener('message', (evt) => {
+  libp2p.services.pubsub.addEventListener('message', (evt) => {
     log(`${evt.detail.from}: ${new TextDecoder().decode(evt.detail.data)} on topic ${evt.detail.topic}`)
   })
-  await helia.libp2p.services.pubsub.subscribe(pubsubTopic)
+  await libp2p.services.pubsub.subscribe(pubsubTopic)
 
   // pubsub pub
   setInterval(() => {
-    helia.libp2p.services.pubsub.publish(pubsubTopic, new TextEncoder().encode(`demo message from browser ${helia.libp2p.peerId}`)).catch(console.error)
+    libp2p.services.pubsub.publish(pubsubTopic, new TextEncoder().encode(`demo message from browser ${libp2p.peerId}`)).catch(console.error)
   }, 2000)
 })()
 
